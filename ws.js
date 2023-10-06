bd6095ef260b23271bf11e0565972b5e73230bcb
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2023-10-06 11:24:30 +0300

    add websocket example

---
+++ ws.js
@@ -0,0 +1,9 @@
+const setupMongoConnection = require('./src/utils/setupMongoConnection');
+const createDirIfNotExists = require('./src/utils/createDirIfNotExists');
+const { UPLOAD_DIR } = require('./src/constants/common');
+
+(async () => {
+  await setupMongoConnection();
+  await createDirIfNotExists(UPLOAD_DIR);
+  require('./src/adapters/ws');
+})();
